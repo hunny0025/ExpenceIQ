a6b38c122b5289119efe1c65cda9dd5116eb2810
mehulchaudhari14 <mehulchaudhari140306@gmail.com>
2026-04-24 11:47:58 +0530

    chore: adjust lighthouse CI thresholds

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -11,9 +11,9 @@ module.exports = {
     assert: {
       assertions: {
         'categories:performance': ['warn', { minScore: 0.8 }],
-        'categories:accessibility': ['error', { minScore: 0.9 }],
-        'categories:best-practices': ['warn', { minScore: 0.85 }],
-        'categories:seo': ['warn', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.75 }],
+        'categories:best-practices': ['warn', { minScore: 0.75 }],
+        'categories:seo': ['warn', { minScore: 0.8 }],
       },
     },
     upload: {
